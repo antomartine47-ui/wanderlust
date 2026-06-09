1d0e22a10ef2d857eaae849e67b6f46db17a3224
antomartine47-ui <antomartine47@gmail.com>
2026-06-09 19:05:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs      = require('fs');
 const app  = express();
 const PORT = process.env.PORT || 3000;
 const DB_FILE = path.join(__dirname, 'db', 'wanderlust.json');
-const PUBLIC_DIR = path.join(__dirname, 'publico');
+const PUBLIC_DIR = path.join(__dirname, 'public');
 
 app.use(cors());
 app.use(express.json());
